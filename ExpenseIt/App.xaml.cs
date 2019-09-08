f2373a5102d43b94ecbdb0f3520e5d374db8c3de
Rick Wilde <rkwilde@gmail.com>
2019-09-07 18:04:14 -0600

    dotnet wpf desktop tutorial

---
+++ ExpenseIt/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ExpenseIt
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+    public class SomeStuffToTest
+    {
+        public SomeStuffToTest() { }
+        public int Add(int a, int b)
+        {
+            int x = a + b;
+            return x;
+        }
+        public int Subtract(int a, int b)
+        {
+            int x = a - b;
+            return x;
+        }
+    }
+}
